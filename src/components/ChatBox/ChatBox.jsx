8c6ce6a00b4a384badb45fa02646d582a933efa7
Gyanthakur <gps.96169@gmail.com>
2024-12-26 00:52:41 +0530

    update: chatbox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,53 +1,83 @@
-import React from 'react'
-import './ChatBox.css'
-import assets from '../../assets/assets'
+import React, { useContext, useEffect, useState } from "react";
+import "./ChatBox.css";
+import assets from "../../assets/assets";
+import { AppContext } from "../../context/AppContext";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../../config/firebase";
 const ChatBox = () => {
-  return (
-    <div className='chat-box'>
-        <div className="chat-user">
-            <img src={assets.profile_img} alt="" />
-            <p>Richard Sanford <img className='dot' src={assets.green_dot} alt="" /></p>
-            <img src={assets.help_icon} className='help' alt="" />
-        </div>
+	const { userData, messagesId, chatUser, messages, setMessages } =
+		useContext(AppContext);
 
-        <div className="chat-msg">
-            <div className="s-msg">
-                <p className="msg">Lorem ipsum dolor sit amet usto praesentium modi....</p>
-                <div>
-                    <img src={assets.profile_img} alt="" />
-                    <p>02:30 PM</p>
-                </div>
-            </div>
-            {/* sender msg */}
-            <div className="s-msg">
-                <img className='msg-img' src={assets.pic1} alt="" />
-                <div>
-                    <img src={assets.profile_img} alt="" />
-                    <p>02:30 PM</p>
-                </div>
-            </div>
-            <div className="r-msg">
-                <p className="msg">Lorem ipsum dolor  elit. Iusto praesentium modi....</p>
-                <div>
-                    <img src={assets.profile_img} alt="" />
-                    <p>02:30 PM</p>
-                </div>
-            </div>
-        </div>
+        const [input, setInput] = useState("");
 
 
+        useEffect(()=>{
+            if(messagesId){
+                const unSub = onSnapshot(doc(db,'messages', messagesId),(res)=>{
+                    setMessages(res.data().messages.reverse())
+                    console.log(res.data().messages.reverse());
+                    
+                })
+                return ()=>{
+                    unSub();
+                }
+            }
+        },[messagesId])
 
-        <div className="chat-input">
-            <input type="text" placeholder='Send a message' />
-            <input type="file" id='image' accept='image/png, image/jpeg'  hidden/>
-            <label htmlFor="image">
-                <img src={assets.gallery_icon} alt="" />
-            </label>
-            <img src={assets.send_button} alt="" />
-        </div>
-      
-    </div>
-  )
-}
+	return chatUser ?  (
+		<div className="chat-box">
+			<div className="chat-user">
+				{/* <img src={assets.profile_img} alt="" /> */}
+				<img src={chatUser.userData.avatar} alt="" />
+				<p>
+					{chatUser.userData.name}<img className="dot" src={assets.green_dot} alt="" />
+				</p>
+				<img src={assets.help_icon} className="help" alt="" />
+			</div>
 
-export default ChatBox
+			<div className="chat-msg">
+				<div className="s-msg">
+					<p className="msg">
+						Lorem ipsum dolor sit amet usto praesentium modi....
+					</p>
+					<div>
+						<img src={assets.profile_img} alt="" />
+						<p>02:30 PM</p>
+					</div>
+				</div>
+				{/* sender msg */}
+				<div className="s-msg">
+					<img className="msg-img" src={assets.pic1} alt="" />
+					<div>
+						<img src={assets.profile_img} alt="" />
+						<p>02:30 PM</p>
+					</div>
+				</div>
+				<div className="r-msg">
+					<p className="msg">
+						Lorem ipsum dolor elit. Iusto praesentium modi....
+					</p>
+					<div>
+						<img src={assets.profile_img} alt="" />
+						<p>02:30 PM</p>
+					</div>
+				</div>
+			</div>
+
+			<div className="chat-input">
+				<input type="text" placeholder="Send a message" />
+				<input type="file" id="image" accept="image/png, image/jpeg" hidden />
+				<label htmlFor="image">
+					<img src={assets.gallery_icon} alt="" />
+				</label>
+				<img src={assets.send_button} alt="" />
+			</div>
+		</div>
+	)
+    : <div className="chat-welcome">
+        <img src={assets.logo_icon} alt="" />
+        <p>Chat anytime, from any where!</p>
+    </div> ;
+};
+
+export default ChatBox;
