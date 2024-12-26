1be5a1346510737bc6dea47afddc85ab6acce870
Gyanthakur <gps.96169@gmail.com>
2024-12-27 00:28:45 +0530

    update: right sidebar

--- src/components/RightSidebar/RightSidebar.jsx
+++ src/components/RightSidebar/RightSidebar.jsx
@@ -1,34 +1,69 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./RightSidebar.css";
 import assets from "../../assets/assets";
 import { logout } from "../../config/firebase";
+import { AppContext } from "../../context/AppContext";
 const RightSidebar = () => {
-	return (
+
+
+	const {chatUser, messages} = useContext(AppContext);
+	const [msgImages, setMsgImages] = useState([])
+	useEffect(()=>{
+		let tempVar = [];
+		messages.map((msg)=>{
+			if(msg.image){
+				tempVar.push(msg.image);
+			}
+
+		})
+		setMsgImages(tempVar);
+		
+
+	},[messages])
+
+	return chatUser ? (
 		<div className="rs">
 			<div className="rs-profile">
 				<img src={assets.profile_img} alt="" />
+				{/* <img src={chatUser.userData.avatar} alt="" /> */}
+
 				<h3>
-					Richard Sanford <img src={assets.green_dot} className="dot" alt="" />
+					{chatUser.userData.name} <img src={assets.green_dot} className="dot" alt="" />
 				</h3>
-				<p>Hey, There i am Richard Sanford Using chat app</p>
+				<p>{chatUser.userData.bio} </p>
 			</div>
 			<hr />
 
 			<div className="rs-media">
 				<p>Media</p>
 				<div>
-					<img src={assets.pic1} alt="" />
-					<img src={assets.pic2} alt="" />
-					<img src={assets.pic3} alt="" />
-					<img src={assets.pic4} alt="" />
-					<img src={assets.pic1} alt="" />
-					<img src={assets.pic2} alt="" />
+					{msgImages.map((url,index)=> (<img onClick={()=>window.open(url)} key={index} src={url} alt="" />))}
+					<img onClick={()=>window.open(assets.pic1)} src={assets.pic1} alt="" />
+					<img onClick={()=>window.open(assets.pic2)} src={assets.pic2} alt="" />
+					<img onClick={()=>window.open(assets.pic3)} src={assets.pic3} alt="" />
+					<img onClick={()=>window.open(assets.pic4)} src={assets.pic4} alt="" />
+					<img onClick={()=>window.open(assets.pic1)} src={assets.pic1} alt="" />
+					<img onClick={()=>window.open(assets.pic2)} src={assets.pic2} alt="" />
+					<img onClick={()=>window.open(assets.pic2)} src={assets.pic2} alt="" />
+					<img onClick={()=>window.open(assets.pic3)} src={assets.pic3} alt="" />
+					<img onClick={()=>window.open(assets.pic4)} src={assets.pic4} alt="" />
+					<img onClick={()=>window.open(assets.pic1)} src={assets.pic1} alt="" />
+					<img onClick={()=>window.open(assets.pic2)} src={assets.pic2} alt="" />
+
+
 				</div>
 			</div>
 
             <button onClick={()=>logout()}>Logout</button>
 		</div>
-	);
+	)
+	:
+	( 
+		<div className="rs">
+			<button onClick={()=>logout()}>Logout</button>
+
+		</div>
+	)
 };
 
 export default RightSidebar;
