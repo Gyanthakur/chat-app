0ef5c8e5b37d99cc1dd63ed4a65751e8fcaea0ac
Gyanthakur <gps.96169@gmail.com>
2024-12-29 00:04:39 +0530

    feat: added loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,6 +4,7 @@ import "./Loader.css";
 const Loader = () => {
   return (
     <div className="loader-container">
+      <p className="loading">Loading...</p>
       <div className="spinner"></div>
     </div>
   );
